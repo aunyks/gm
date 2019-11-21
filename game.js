c881936d4b2fbcae3604b8839eb404c59f39409c
Gerald Nash <me@aunyks.com>
2019-11-21 10:25:15 -0500

    spruce up

--- game.js
+++ game.js
@@ -5,6 +5,11 @@ function setup() {
   cnv.parent('sketch-container')
 }
 
+function sleep(miliseconds) {
+  const currentTime = new Date().now();
+  while (currentTime + miliseconds >= new Date().now()) { }
+}
+
 const settings = {
   environment: {
     thickness: 80, // px
@@ -70,11 +75,18 @@ const gameState = {
     distanceTraveled: 0
   },
   environment: {
-    world: 'dungeon', // dungeon | grass | beach
+    world: 'dungeon', // dungeon | grass | beach | failure | success
     backgroundPieces: createDungeonBg(10)
   }
 }
 
+alert('Welcome to Underworlds!')
+alert('To win the game, advance to all of the levels.')
+alert('To advance levels, answer the prompts correctly.')
+alert('To get prompted, move enough meters in a direction. Good luck!')
+alert('Stan:\n\nI\'m Stan, and I\'ve trapped your friend Nathan in my dungeon world.\n\nI think he\'s trying to get out, but every time he moves far enough he\'s prompted with a question that he can\'t answer!\n\nI\'m so smart haha')
+let gameRendered = -60
+let nathanGreeted = false
 let ctr = 0
 function draw() {
   clear()
@@ -104,6 +116,18 @@ function draw() {
       stroke(251, 151, 109)
       rect(0 - environment.edgeThickness, 0 - environment.edgeThickness, CANVAS_WIDTH + 2 * environment.edgeThickness, environment.thickness)
       rect(0 - environment.edgeThickness, CANVAS_HEIGHT - environment.thickness, CANVAS_WIDTH + 2 * environment.edgeThickness, CANVAS_HEIGHT)
+      break;
+    case 'failure':
+      background(0)
+      fill(255)
+      text(`You've failed.\n\nIf you want to amount to\nsomething better, refresh the page.`, 20, 140)
+      sleep(30 * 60 * 1000)
+      break;
+    case 'success':
+      background(255)
+      fill(0)
+      text(`You've succeeded!\n\nYour effort is spectacular.`, 20, 140)
+      sleep(30 * 60 * 1000)
       break;
     default:
       alert(`Something tried to change the environment to "${world}", which doesn't exist`)
@@ -235,6 +259,50 @@ function draw() {
     }
   }
   nathan.distanceTraveled += xMovementRate
+
+  textSize(18)
+  text(`Meters: ${nathan.distanceTraveled}`, 20, 30)
+
+  if (world === 'dungeon' && nathan.distanceTraveled >= 500) {
+    if (prompt('How many 1s are in 2?').trim() === '2') {
+      gameState.environment.world = 'grass'
+      gameState.environment.backgroundPieces = createGrassBg(5)
+      nathan.distanceTraveled = 0
+      alert(`Stan:\n\nWhat?! That's preposterous? How'd he answer it right?\n\nAh, goodness. It was you wasn't it? Well, good luck getting out of the next few worlds.`)
+      alert(`Nathan:\n\nOmg good job! Now it looks like we're in a grassy world. I think I remember my friend saying to go 1000 backward to get prompted..`)
+    } else {
+      alert('Incorrect')
+      gameState.environment.world = 'failure'
+    }
+  } else if (world === 'grass' && nathan.distanceTraveled <= -1000) {
+    if (prompt(`What's 1 + 2?`).trim() === '3') {
+      gameState.environment.world = 'beach'
+      gameState.environment.backgroundPieces = []
+      nathan.distanceTraveled = 0
+      alert(`Hm, not bad. Next is the beach world. Travel 2000 meters forward to win the game.`)
+    } else {
+      alert('Incorrect')
+      gameState.environment.world = 'failure'
+    }
+  } else if (world === 'beach' && nathan.distanceTraveled >= 2000) {
+    if (prompt(`What's the first letter of the alphabet?`).trim().toUpperCase() === 'A') {
+      gameState.environment.world = 'beach'
+      gameState.environment.backgroundPieces = []
+      nathan.distanceTraveled = 0
+      alert(`Spectacular.`)
+      gameState.environment.world = 'success'
+    } else {
+      alert('Incorrect')
+      gameState.environment.world = 'failure'
+    }
+  } else { }
+  if (gameRendered > 1 && !nathanGreeted) {
+    alert(`Nathan:\n\nHey! HEY! You there! Help me!\n\nI'm trapped in this weird dungeon but every time I look for an exit I get hit with a tough question.\n\nAnd then when I answer it wrong some Stan guy laughs and says "I'm so smart" and does this ugly laugh thing. Please help me answer them so I can leave!`)
+    alert('Nathan:\n\nI remember getting prompted last time I moved 500 meters forward. Let\'s try that again..')
+    nathanGreeted = true
+  } else {
+    gameRendered += 1
+  }
 }
 
 function touchStarted() {
